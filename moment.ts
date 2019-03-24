a3ca08e5a4bc2bc0ffe557e365114ed82b0522e2
lisniuse <17560235@qq.com>
2019-03-24 20:48:50 +0800

    Updated comment.

--- moment.ts
+++ moment.ts
@@ -1,8 +1,6 @@
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
 import "./vendor/moment.js";
 
-// TODO: provide decent type declarions for these
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const { moment } = window as any;
 
 export { moment };
